c29a483e683792762f46d214aceccb387dddfa52
Maryam-n98 <mariam.mohannad98@gmail.com>
2021-08-12 22:22:57 +0300

    lab26

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(View view) {
+        Intent addIntent = new Intent(this, AddTask.class);
+        startActivity(addIntent);
+    }
+
+    public void all(View view) {
+        Intent allIntent = new Intent(this, AllTasks.class);
+        startActivity(allIntent);
     }
 }
